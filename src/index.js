4b766b5546ff5e901cfab4e71f28f9e4d82b86df
pt-br <ltavares@yieldstreet.com>
2018-08-02 20:23:20 -0300

    Install & config a bunch of essential libs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
